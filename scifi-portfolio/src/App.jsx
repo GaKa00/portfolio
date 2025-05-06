9440446375c28ee05d672cc1285bf4350301bf4e
Gabriel Kassarp <gkassarp@gmail.com>
2025-05-06 13:12:03 +0200

    init

---
+++ scifi-portfolio/src/App.jsx
@@ -0,0 +1,19 @@
+import TerminalIntro from "./components/TerminalIntro";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import TechStack from "./components/Techstack";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <div>
+      <TerminalIntro />
+      <About />
+      <Projects />
+      <TechStack />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
