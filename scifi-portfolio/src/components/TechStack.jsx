601997b1cd32939b0a274c0ebea66f3000c80fc0
Gabriel Kassarp <gkassarp@gmail.com>
2025-05-06 14:19:52 +0200

    addedd techstack info

--- scifi-portfolio/src/components/TechStack.jsx
+++ scifi-portfolio/src/components/TechStack.jsx
@@ -1,7 +1,40 @@
 import React from 'react'
+import '../styles/Techstack.css'
 
 export default function Techstack() {
   return (
-    <div>Techstack</div>
+    <div>
+        <details className='techstack' open>
+            <summary className='techstack-summary'>Tech Stack</summary>
+            <div className='techstack-content'>
+                <h4>Frontend Capabilities</h4>
+                <ul className='techstack-list'>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                    <li>JavaScript</li>
+                    <li>TypeScript</li>
+                    <li>React.js</li>
+                    <li>Vue</li>
+                    <li>Next.js</li>
+                    <li>Nuxt</li>
+                    <li>Tailwind CSS</li>
+                    </ul>
+                <h4>Backend Capabilities</h4>
+                    <ul className='techstack-list'>
+                    <li>Node.js</li>
+                    <li>Express.js</li>
+                    <li>MongoDB</li>
+                    <li>PostgreSQL</li>
+                    <li>Prisma</li>
+                    <li>Firebase</li>
+                    <li>Supabase</li>
+                   </ul>
+
+
+            </div>
+
+
+        </details>
+    </div>
   )
 }
