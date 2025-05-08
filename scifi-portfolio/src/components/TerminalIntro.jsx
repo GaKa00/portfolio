299275a11f66c8db990e41969614f932d0a5d0e8
Gabriel Kassarp <gkassarp@gmail.com>
2025-05-08 15:59:16 +0200

    updated terminal intro

--- scifi-portfolio/src/components/TerminalIntro.jsx
+++ scifi-portfolio/src/components/TerminalIntro.jsx
@@ -1,17 +1,21 @@
 // src/components/TerminalIntro.jsx
 import { useEffect, useState } from "react";
+import { Typewriter } from 'react-simple-typewriter'
+
 import "./styles/terminal.css"
 
 const lines = [
   "> INITIATE EMPLOYER.DISTRESS.PING...",
-  "Signal acquired.",
-  "Loading reciever data...",
-  "Reciever data loaded.",
-  "Ranking: Tech Priest Neophyte",
-  "Subject: Gabriel Kassarp",
-  "Classification: Fullstack Developer",
-  "Recieving data burst...",
-  "Data burst recieved, uploading profile..."
+  "  Signal acquired.",
+  "  Loading reciever data...",
+  "  Reciever data loaded.",
+  "  Ranking: Tech Priest Neophyte",
+  "  Subject: Gabriel Kassarp",
+  "  Classification: Fullstack Developer",
+  "  Recieving data burst...",
+  "  Data burst recieved, uploading profile...",
+  ""
+  
 ];
 
 export default function TerminalIntro({ onFinish }) {
@@ -42,9 +46,19 @@ export default function TerminalIntro({ onFinish }) {
 )}
 
       {displayedLines.map((line, i) => (
-        <div key={i} className="typed-line">{line}</div>
+      <div key={i} className="typed-line">
+      <Typewriter
+        words={[line]}
+        loop={1}
+        cursor={false}
+        typeSpeed={30}
+        deleteSpeed={0}
+        delaySpeed={0}
+      />
+    </div>
+
       ))}
-      {lineIndex < lines.length && <div className="typed-line cursor">█</div>}
+      {lineIndex >0 && lineIndex < lines.length &&( <div className="typed-line cursor">█</div>)}
     </div>
   );
 }
