958fa8b533c4525336a9ebcf188f630fbb51e13b
Gabriel Kassarp <gkassarp@gmail.com>
2025-05-09 17:17:25 +0200

    made image show

--- scifi-portfolio/src/components/About.jsx
+++ scifi-portfolio/src/components/About.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
+import React from "react";
+import "./styles/about.css"
 
 export default function About() {
   return (
-    <div>
-      <div>
-        <div className="border border-green-500 p-4">
-          <img src="../assets/mechanicus.png"/>
-          <h3>Additional Info</h3>
-          <p>
-            {" "}
-            The neophyte shows promise and fast learning in the many languages
-            of the cult mechanicus. Neophyte Gabriel has presented satisfactory
-            social skills and communications with peers.{" "}
-          </p>
-          <br></br> Neophyte Gabriel has shown a strong interest in the
-          following areas:
-          <ul>
-            <li>Video Games</li>
-            <li>Warhammer 40K</li>
-            <li>History</li>
-            <li>Cooking</li>
-            <li>Juggling</li>
-          </ul>
+    <div className="about-container">
+      <div className="about-box">
+        <div className="image-wrapper">
+          <img
+            src="../src/assets/mechanicus.png"
+            alt="Mechanicus Symbol"
+            className="about-image"
+          />
+        </div>
+        <div className="info-wrapper">
+        <h3>Additional Info</h3>
+        <p>
+          The neophyte shows promise and fast learning in the many languages of
+          the Cult Mechanicus. Neophyte Gabriel has presented satisfactory
+          social skills and communication with peers.
+        </p>
+        <p>
+          Neophyte Gabriel has shown a strong interest in the following areas:
+        </p>
+        <ul>
+          <li>Video Games</li>
+          <li>Warhammer 40K</li>
+          <li>History</li>
+          <li>Cooking</li>
+          <li>Juggling</li>
+        </ul>
+
         </div>
       </div>
     </div>
